c6ac7357edeaa15f8c730ac286e4b54f5f01e8e7
Taha KARABIYIK <tahakrb275@gmail.com>
2024-03-24 17:43:39 +0300

    source code

---
+++ Assets/Scripts/Frıuit/ThrowFruitController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowFruitController : MonoBehaviour
+{
+    public static ThrowFruitController instance;
+
+    public GameObject CurrentFruit { get; set; }
+
+    [SerializeField] private Transform _fruitTransform;
+    [SerializeField] private Transform _parentAfterThrow;
+    [SerializeField] private FruitSelector _selector;
+
+    private PlayerController _playerController;
+
+    private Rigidbody2D _rb;
+    private CircleCollider2D _circleCollider;
+
+    public Bounds Bounds { get; private set; }
+
+    private const float EXTRA_WIDTH = 0.03f;
+
+    public bool CanThrow { get; set; } = true;
+
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+
+    }
+
+    private void Start()
+    {
+        _playerController = GetComponent<PlayerController>();
+
+        SpawnAFruit(_selector.PickRandomFruitForThrow());
+    }
+
+    private void Update()
+    {
+        if (UserInput.IsThrowPressed && CanThrow)
+        {
+            SpriteIndex index = CurrentFruit.GetComponent<SpriteIndex>();
+            Quaternion rot = CurrentFruit.transform.rotation;
+
+            GameObject go = Instantiate(FruitSelector.instance.Fruits[index.Index], CurrentFruit.transform.position, rot);
+            go.transform.SetParent(_parentAfterThrow);
+
+            Destroy(CurrentFruit);
+
+            CanThrow = false;
+        }
+
+    }
+
+    public void SpawnAFruit(GameObject fruit)
+    {
+        GameObject gameObj = Instantiate(fruit, _fruitTransform);
+        CurrentFruit = gameObj;
+
+        _circleCollider = CurrentFruit.GetComponent<CircleCollider2D>();
+        Bounds = _circleCollider.bounds;
+
+        _playerController.ChangeBoundary(EXTRA_WIDTH); // sýnýrlarý yeniden güncelleriz
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+##Gameplay
+
+ [Gameplay video in youtube](https://www.youtube.com/watch?v=yDzWMab-hYI&list=PLvxc8U8HNrmoNPtNoX33UAXoAfcNrYoFe&index=22&ab_channel=TahaKARABIYIK).
